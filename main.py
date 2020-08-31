d137dc02d45b22c6fdd6355ff193b9950206af0f
Bageljr <aryanagarwal@my.unt.edu>
2020-08-31 01:16:50 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+# bot.py
+import os
+
+import discord
+from dotenv import load_dotenv
+import asyncio
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+@client.event
+async def on_message(message):
+    if message.content.startswith('$thumb'):
+        channel = message.channel
+        await channel.send('Send me that 👍 reaction, mate')
+
+        def check(reaction, user):
+            return user == message.author and str(reaction.emoji) == '👍'
+
+        try:
+            reaction, user = await client.wait_for('reaction_add', timeout=60.0, check=check)
+        except asyncio.TimeoutError:
+            await channel.send('👎')
+        else:
+            await channel.send('👍')
+
+
+client.run(TOKEN)
